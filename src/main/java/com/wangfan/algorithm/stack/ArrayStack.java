326e1bea4c6a99a08a505a58ca3a456dd00c7e83
wangfan <wangfan980@163.com>
2018-12-23 17:50:53 +0800

    add arraystack

--- src/main/java/com/wangfan/algorithm/stack/ArrayStack.java
+++ src/main/java/com/wangfan/algorithm/stack/ArrayStack.java
@@ -1,7 +1,32 @@
 package com.wangfan.algorithm.stack;
 
+/**
+ * 数组实现栈
+ * params: n是栈大小；count是栈中元素个数
+ */
+
 public class ArrayStack{
 
-	
+	private String[] items;
+
+	private int count = 0;
+
+	private int n = 0;
+
+    public ArrayStack(int n){
+        this.items = new String[n];
+        this.n = n;
+    }
+
+	public boolean push(String item){
+        if(count == n) return false;
+        items[count++] = item;
+        return true;
+    }
+
+    public String pop(){
+        if(count == 0) return null;
+        return items[--count];
+    }
 
 }
